3e23971dddf64a8991f74b0dc822e77ae937f434
Shivam Kumar <shivambspr@gmail.com>
2022-10-18 09:07:25 +0530

    adding sendMessage function

--- Stock_Alert_Notifier.py
+++ Stock_Alert_Notifier.py
@@ -4,6 +4,20 @@ from datetime import datetime
 from timeloop import Timeloop
 from datetime import timedelta
 from twilio.rest import Client
+#code added by Shivam
+
+def sendMessage(text):
+    account_sid = "SID" # Your Account SID from twilio.com/console
+    auth_token  = "Token" # Your Auth Token from twilio.com/console
+
+    client = Client(account_sid, auth_token)
+
+    message = client.messages.create(
+        to="XXX", #enter your verified phone number
+        from_="+14255411934",
+        body=text)
+
+    print(message.sid)
 #code added by Aakshita
 
 tl = Timeloop()
